32a0d911d6e4d42588d65e552daa1c1d2640fdc9
mateoordonezfigueroa-collab <mateoordonezfigueroa@gmail.com>
2026-07-16 20:03:24 -0600

    Add files via upload

---
+++ <!DOCTYPE html>.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+html = """
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title>Página de texto</title>
+</head>
+<body>
+    <h1>Hola</h1>
+    <p>Esta es una página web con texto generada desde Python.</p>
+</body>
+</html>
+"""
+
+print(html)
